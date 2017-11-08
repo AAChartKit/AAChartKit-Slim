f410b22113cbefbbb499999d1b2bb6303d92ac77
李照闫 <2236368544@qq.com>
2017-11-08 15:13:36 +0800

    Initial the project

---
+++ AAChartKit-SlimDemo/AAChartKit-SlimDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AAChartKit-SlimDemo
+//
+//  Created by An An on 2017/11/8.
+//  Copyright © 2017年 An An. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
